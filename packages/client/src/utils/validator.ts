839053a3bb96e572239f40b4489ac48fc9d3a55c
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2022-12-19 22:49:01 +0300

    Исправил маску для пароля (добавил дефис), стили для картинки аватара

--- packages/client/src/utils/validator.ts
+++ packages/client/src/utils/validator.ts
@@ -3,7 +3,7 @@ export const pattern = (): Record<string, RegExp> => {
     name: /^[A-ZА-Я][a-zа-я]+$/,
     email: /\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}/,
     login: /^(?=.*[a-zA-Z])[\w-]{3,20}$/,
-    password: /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[0-9a-zA-Z!@#$%^&*]{8,40}$/,
+    password: /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[0-9a-zA-Z!@#$%^&*-]{8,40}$/,
     phone: /^[+]?[0-9]{10,15}$/,
     message: /[^\s]/,
   }
