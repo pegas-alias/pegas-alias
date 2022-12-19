1446d71dd556d09a256e02b7e9cd07f02bc1707a
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2022-12-19 22:48:16 +0300

    Исправил client_id для OAuth

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,2 +1,3 @@
 export const YA_PRACTICUM_URL = 'https://ya-praktikum.tech/api/v2'
 export const LOCAL_URL = 'https://pegas-alias.ru'
+export const OAUTH_CLIENT_ID = '8a24c917819c46d3b4dfd9dd3d5a4d9c'
