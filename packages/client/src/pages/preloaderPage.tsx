a45bbd0f91dc8637f75cbf5ed231780e3a6a479e
AlexPilinni <alexpilinni@ya.ru>
2022-12-20 13:05:53 +0400

    add preloader-page

---
+++ packages/client/src/pages/preloaderPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Intro } from '../components'
+
+export const PreloaderPage: React.FC = () => {
+  return (
+    <>
+      <div className={'preloader-page'}>
+        <Intro></Intro>
+      </div>
+    </>
+  )
+}
