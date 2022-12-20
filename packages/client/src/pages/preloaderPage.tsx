ad6f2384a47766da1f7e88a89e2e9613887addfb
AlexPilinni <alexpilinni@ya.ru>
2022-12-20 13:31:57 +0400

    fix page

--- packages/client/src/pages/preloaderPage.tsx
+++ packages/client/src/pages/preloaderPage.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import { Intro } from '../components'
 
 export const PreloaderPage: React.FC = () => {
   return (
     <>
-      <div className={'preloader-page'}>
-        <Intro></Intro>
-      </div>
     </>
   )
 }
